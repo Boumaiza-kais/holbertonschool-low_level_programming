01ef48a6891bfbbbbdc846a2ac83213de326a844
boumaiza-kais <1110@holbertonschool.com>
2019-09-24 12:54:38 +0100

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,23 +9,21 @@
 void print_to_98(int n)
 {
 int i;
+
 if (n == 98)
 {
-printf("%d, ", n);
+printf("98\n");
 }
 else if (n < 98)
 {
 for (i = n; i < 98; i++)
-{
 printf("%d, ", i);
-}
+printf("98\n");
 }
 else
 {
 for (i = n; i > 98; i--)
-{
-printf("%d,", i);
-}
-}
+printf("%d, ", i);
 printf("98\n");
+}
 }
