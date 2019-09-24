216f577adb3ef15d8b04d5f37966b0ac8654f545
boumaiza-kais <1110@holbertonschool.com>
2019-09-24 14:54:47 +0100

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first int ; @b: second int ; @c: third int
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
