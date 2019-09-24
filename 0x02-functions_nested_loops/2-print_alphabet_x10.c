4d14dc700e576ee052e575288a683bd72c92e6a3
boumaiza-kais <1110@holbertonschool.com>
2019-09-24 09:45:16 +0100

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,20 +4,19 @@
  * Description : alphabet game
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 char c ;
 int i = 0 ;
 while (i < 10 )
-  {
+    {
     c = 'a';
-  while ( c <= 'z')
+     while ( c <= 'z')
 {
 _putchar (c);
 c++ ;
 }
     _putchar ("\n");
     i++;
-   }  
-return (0);
+    }  
 }
