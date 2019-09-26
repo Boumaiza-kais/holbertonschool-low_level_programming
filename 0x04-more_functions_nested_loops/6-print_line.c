c8e929056a7753c32f27d2d4f38a752c5779bde4
boumaiza-kais <1110@holbertonschool.com>
2019-09-26 12:48:25 +0100

    commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,8 @@
 void print_line(int n)
 {
 int i;
-if (n <= 0)
-{
-_putchar ('\n');
- return;
-} 
-else
-for (i = 0; i < n ; i++)
-{
-_putchar ('_');
-_putchar ('\n');
-}
+for (i = 0; i < n; i++)
+if (n > 0)
+_putchar('_');
+_putchar('\n');
 }
