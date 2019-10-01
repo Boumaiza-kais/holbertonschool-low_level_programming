0226d272a4a64edb4ade7df6651523b0bfe32b00
boumaiza-kais <1110@holbertonschool.com>
2019-10-01 11:43:52 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * rev_string - function that reverses a string.
+ * followed by a new line.
+ * @s : char  type
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+int i;
+int j;
+int k; 
+int len ;
+len=0;
+for (i = 0; s[len] != 0 ; i++)
+{
+len ++ ;  
+}
+ j=len -1;
+for (i = 0; i < len /2 ; i++)
+{
+k;
+}
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * puts_half - function that prints half of a string, followed by a new line
+ * @str : char  type
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+  
+}
