0b2d13dcbd776f3f6b3e50e0113903bf276375e3
boumaiza-kais <1110@holbertonschool.com>
2019-10-01 12:20:23 +0100

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,7 +4,29 @@
  * @str : char  type
  * Return: Always 0.
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-  
+int i;
+int l;
+int n;
+l = 0;
+while ((*(str + l) != '\0'))
+{
+l++;
+}
+n = (l - 1) / 2;
+if (n / 2 == 0)
+{
+n = (l - 1) / 2;
+for (i = n + 1; i <= l - 1 ; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+for (i = n + 1; i <= l - 1 ; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
