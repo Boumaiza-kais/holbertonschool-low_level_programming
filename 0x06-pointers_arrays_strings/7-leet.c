5eb350baa0a3fc1770b03428df151f455c5b1406
boumaiza-kais <1110@holbertonschool.com>
2019-10-03 15:44:42 +0100

    commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+/**
+* leet - encodes a string into 1337
+* @s: string
+*
+* Return: pointer to a string
+*/
+char *leet(char *s)
+{
+int i;
+i = 0;
+int j;
+char *l = "oOlLeEaAtT";
+char *n = "0011334477";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; l[j] != '\0'; j++)
+{
+if (s[i] == l[j])
+s[i] = n[j];
+}
+}
+return (s);
+}
