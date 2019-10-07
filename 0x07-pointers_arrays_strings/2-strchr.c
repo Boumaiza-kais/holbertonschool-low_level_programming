247416dea58778f19247a19aa1779d16680f6e6e
boumaiza-kais <1110@holbertonschool.com>
2019-10-07 16:27:54 +0100

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: char type
+ * @c: char type
+ *
+ * Return: pointer 
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != 0 ; i++)
+{
+if (s[i] == c)
+return (s + i);
+}
+if (s[i] == c)
+return (s + i);
+return (0);
+}
