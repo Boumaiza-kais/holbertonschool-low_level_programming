558dd7f29dad2cab83693096ee16e8ad653bb17e
boumaiza-kais <1110@holbertonschool.com>
2019-10-07 18:38:22 +0100

    commit

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include"holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: char type
+ * @to: char type
+ * Return : void
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
