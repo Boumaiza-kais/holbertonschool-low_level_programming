41071ea80f6be89b26173418efd3ef81f057a02e
boumaiza-kais <1110@holbertonschool.com>
2019-10-10 11:52:04 +0100

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,10 +12,10 @@ int checksqr(int a, int b)
 {
 if (b * b == a)
 return (b);
-else if (b == 1)
+else if (b * b > a)
 return (-1);
 else
-return (checksqr(a, b - 1));
+return (checksqr(a, b + 1));
 }
 
 /**
@@ -28,5 +28,5 @@ int _sqrt_recursion(int n)
 if (n < 0)
 return (-1);
 else
-return (checksqr(n, (n / 2)));
+return (checksqr(n, 0));
 }
