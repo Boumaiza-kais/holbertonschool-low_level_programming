db02f23bfa339dca5efe598b1148df569fb9e97f
boumaiza-kais <1110@holbertonschool.com>
2019-10-10 11:32:59 +0100

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* checksqr - fills memory with a constant byte.
+* @a: int type
+* @b: int type
+* Return: 0.
+*/
+
+int checksqr(int a, int b)
+{
+if (b * b == a)
+return (b);
+else if (b == 1)
+return (-1);
+else
+return (checksqr(a, b - 1));
+}
+
+/**
+* _sqrt_recursion - fills memory with a constant byte.
+* @n: int type
+* Return: 0.
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else
+return (checksqr(n, (n / 2)));
+}
