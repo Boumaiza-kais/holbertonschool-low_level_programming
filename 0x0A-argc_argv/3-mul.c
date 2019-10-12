fcbcf30fff1122ec04d643432f6a6e92db7090e3
boumaiza-kais <1110@holbertonschool.com>
2019-10-12 22:35:16 +0100

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: number of command line arguments
+ * @argv: an array of size argc
+ *
+ * Return: return 0 ; if there is error return 1
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+puts("Error");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
