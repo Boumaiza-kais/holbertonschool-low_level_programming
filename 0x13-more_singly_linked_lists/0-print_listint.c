ca01ceda3327511f6269e1a8d3aa6726327bff0f
boumaiza-kais <1110@holbertonschool.com>
2019-11-05 18:11:48 +0100

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t i = 0;
+unsigned int i = 0;
 while (h != NULL)
 {
-printf("%d \n", h->n);
-i++;
+printf("%d\n", h->n);
 h = h->next;
+i++;
 }
 return (i);
 }
