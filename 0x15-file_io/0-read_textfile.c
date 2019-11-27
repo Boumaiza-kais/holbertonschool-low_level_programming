e6294b9910fd1eb5f1607553cbb750afab22e614
boumaiza-kais <1110@holbertonschool.com>
2019-11-27 13:38:00 +0100

    commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * read_textfile - function that reads a text file
+ * and prints it to the POSIX standard output.
+ * @filename: const char type
+ * @letters:  size_t type
+ * Return: always success
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t fw;
+ssize_t fr;
+char *ptr;
+int fd;
+
+if (filename == NULL)
+return (0);
+ptr = malloc(letters * sizeof(char));
+if (ptr == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+fr = read(fd, ptr, letters);
+if (fdr == -1)
+return (0);
+*(ptr + fr) = '\0';
+fdw = write(STDOUT_FILENO, p, fr);
+if (fw == -1 || fw != fr)
+return (0);
+close(fd);
+return (fw);
+}
