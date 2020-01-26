17afd515f6a5de478c089e8bbffa576879006925
boumaiza-kais <1110@holbertonschool.com>
2020-01-26 14:16:42 +0100

    fix 5 and 6

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,24 +7,24 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *tmp;
-	unsigned int i;
-	int k = 0;
+hash_node_t *tmp;
+unsigned int i;
+int k = 0;
 
-	if (!ht)
-		return;
-	printf("{");
-	for (i = 0; i < ht->size; i++)
-	{
-		temp = ht->array[index];
-		while (temp != NULL)
-		{
-			if (k == 0)
-                        printf(", ");
-			printf("'%s': '%s'", temp->key, temp->value);
-			temp = temp->next;
-			k = 1;
-		}
-	}
-	printf("}\n");
+if (!ht)
+return;
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+temp = ht->array[i];
+while (temp != NULL)
+{
+if (k == 0)
+printf(", ");
+printf("'%s': '%s'", temp->key, temp->value);
+temp = temp->next;
+k = 1;
+}
+}
+printf("}\n");
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,22 +7,22 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *temp;
-	unsigned int i = 0;
+hash_node_t *temp;
+unsigned int i = 0;
 
-	if (!ht)
-		return;
-	while (i < ht->size)
-	{
-		temp = ht->array[index];
-		while (temp != NULL)
-		{
-			free(temp->key);
-			free(temp->value);
-			free(temp);
-		}
-		i++;
-	}
-	free(ht->array);
-	free(ht);
+if (!ht)
+return;
+while (i < ht->size)
+{
+temp = ht->array[i];
+while (temp != NULL)
+{
+free(temp->key);
+free(temp->value);
+free(temp);
+}
+i++;
+}
+free(ht->array);
+free(ht);
 }
