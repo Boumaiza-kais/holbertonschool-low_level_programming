807bfec9d1109ebddab1f97643ae4cd7593fdc87
boumaiza-kais <1110@holbertonschool.com>
2020-01-26 13:42:28 +0100

    commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int i;
 hash_node_t *new_node;
 hash_node_t *tmp_node;
-if (key == NULL || ht == NULL || value == NULL || *key == '\0')
+if (key == NULL || ht == NULL || value == NULL || strcmp(key, "") == 0)
 return (0);
 i = key_index((unsigned char *)key, ht->size);
 tmp_node = ht->array[i];
@@ -25,6 +25,7 @@ free(tmp_node->value);
 tmp_node->value = strdup(value);
 return (1);
 }
+tmp_node = tmp_node->next;
 }
 new_node = malloc(sizeof(hash_node_t *));
 if (new_node == NULL)
@@ -34,10 +35,5 @@ new_node->value = strdup(value);
 new_node = NULL;
 new_node = ht->array[i];
 ht->array[i] = new_node;
-}
-else
-{
-ht->array[i] = new_node;
-}
 return (1);
 }
