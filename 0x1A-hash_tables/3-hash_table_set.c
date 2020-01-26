8cf74f3ca83bbfcc82924ae3104f9177b657f961
boumaiza-kais <1110@holbertonschool.com>
2020-01-26 13:12:13 +0100

    commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,29 +10,29 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int i;
-        hash_node_t *new_node, *tmp_node;
-
-	if (key == NULL || ht == NULL || value == NULL || *key == '\0')
-		return (0);
-	i = key_index((unsigned char *)key, ht->size);
-        tmp_node = ht->array[i];
-	for (; tmp_node != NULL; tmp_node = tmp_node->next)
-	{
-		if (!strcmp(tmp_node->key, key))
-		{
-			free(tmp_node->value);
-			tmp_node->value = strdup(value);
-			return (1);
-		}
-	}
-	new_node = malloc(sizeof(hash_node_t*));
-	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node = NULL;
-        new_node = ht->array[i];
-	ht->array[i] = new_node;
-	return (1);
+unsigned long int i;
+hash_node_t *new_node;
+hash_node_t *tmp_node;
+if (key == NULL || ht == NULL || value == NULL || *key == '\0')
+return (0);
+i = key_index((unsigned char *)key, ht->size);
+tmp_node = ht->array[i];
+for (; tmp_node != NULL; tmp_node = tmp_node->next)
+{
+if (!strcmp(tmp_node->key, key))
+{
+free(tmp_node->value);
+tmp_node->value = strdup(value);
+return (1);
+}
+}
+new_node = malloc(sizeof(hash_node_t *));
+if (new_node == NULL)
+return (0);
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node = NULL;
+new_node = ht->array[i];
+ht->array[i] = new_node;
+return (1);
 }
